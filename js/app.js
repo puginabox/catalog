fc0d5706d5440a24325e67470513000060f935f6
Griffin Byron <puginabox@gmail.com>
2014-12-03 14:50:02 -0500

    adding ng-repeat

--- js/app.js
+++ js/app.js
@@ -2,12 +2,25 @@ console.log("1 - app");
 
 var app = angular.module('workflow', ['ngSanitize', 'ngRoute']);
 
-app.config(['$routeProvider', function($routeprovider){
-  $routeprovider.when('/page1', {
-      templateUrl: 'templates/page1.html',
-      controller: 'page1Controller'
-  }); 
-    $routeprovider.otherwise({
-        redirectTo: '/page1'
-    });
+app.config(['$routeProvider',
+    function ($routeprovider) {
+        $routeprovider.when('/page1', {
+            templateUrl: 'templates/page1.html',
+            controller: 'page1Controller'
+        });
+        $routeprovider.when('/page2', {
+            templateUrl: 'templates/page2.html',
+            controller: 'page2Controller'
+        });
+        $routeprovider.when('/page3', {
+            templateUrl: 'templates/page3.html',
+            controller: 'page3Controller'
+        });
+        $routeprovider.when('/page4', {
+            templateUrl: 'templates/page4.html',
+            controller: 'page4Controller'
+        });
+        $routeprovider.otherwise({
+            redirectTo: '/page1'
+        });
 }]);
--- js/controllers/page1Controller.js
+++ js/controllers/page1Controller.js
@@ -4,26 +4,37 @@
 console.log("2 - page1Controller");
 
 app.controller('page1Controller', function page1Controller($scope) {
-        $scope.object = {
-            'name': 'dude',
-            'color': 'cobalt'
-        };
-            $scope.navigation = [
-                    {
-                        page: '/page1',
-                        title: 'Page One'
-                    },
-                    {
-                        page: '/page2',
-                        title: 'Page Two'
-                    },
-                    {
-                        page: '/page3',
-                        title: 'Page Three'
-                    },
-                    {
-                        page: '/page4',
-                        title: 'Page Four'
-                    }
-            ];
+    $scope.object = [
+        {
+            'name': 'pansey',
+            'color': 'yellow'
+    }, {
+            'name': 'tulip',
+            'color': 'scarlet'
+    }, {
+            'name': 'hyacinth',
+            'color': 'white'
+    }, {
+            'name': 'rose',
+            'color': 'wine'
+    }
+    ];
+    $scope.navigation = [
+        {
+            page: '/page1',
+            title: 'Page One'
+                },
+        {
+            page: '/page2',
+            title: 'Page Two'
+                },
+        {
+            page: '/page3',
+            title: 'Page Three'
+                },
+        {
+            page: '/page4',
+            title: 'Page Four'
+                }
+        ];
 });
